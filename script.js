af364dd3cc7638f22832f73d6da83f1c397cf265
fshippman <fshippman@yahoo.com>
2023-08-16 15:13:27 +0200

    added dropdown menu

--- script.js
+++ script.js
@@ -123,24 +123,31 @@ function createPokemonCardHTML(pokemon) {
                     <div class="pokemon-type-tag " style = "background-color: ${findColorTag(pokemon, type)}">${type}</div>
                     ${secondType}  
                     </div>
-                    
-                </div>
-                <div class="pokemonImage-container">
+                    <div class="pokemonImage-container">
                         <div class="pokemonImage-background" style = "background-color: ${findColorTag(pokemon, type)}">
                             <img class="pokemonImage" src="${pokemon['sprites']['other']['official-artwork']['front_default']}"/>
                         </div>
-            </div>
+                </div>
+                </div>
+                
 
             <div class="detail-footer">
             
                 <div class="navigation-container">
-                    <svg class="arrow" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.978 14.999v3.251c0 .412-.335.75-.752.75-.188 0-.375-.071-.518-.206-1.775-1.685-4.945-4.692-6.396-6.069-.2-.189-.312-.452-.312-.725 0-.274.112-.536.312-.725 1.451-1.377 4.621-4.385 6.396-6.068.143-.136.33-.207.518-.207.417 0 .752.337.752.75v3.251h9.02c.531 0 1.002.47 1.002 1v3.998c0 .53-.471 1-1.002 1zm-1.5-7.506-4.751 4.507 4.751 4.507v-3.008h10.022v-2.998h-10.022z" fill-rule="nonzero"/></svg>
-                
+                <svg class="arrow" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.978 14.999v3.251c0 .412-.335.75-.752.75-.188 0-.375-.071-.518-.206-1.775-1.685-4.945-4.692-6.396-6.069-.2-.189-.312-.452-.312-.725 0-.274.112-.536.312-.725 1.451-1.377 4.621-4.385 6.396-6.068.143-.136.33-.207.518-.207.417 0 .752.337.752.75v3.251h9.02c.531 0 1.002.47 1.002 1v3.998c0 .53-.471 1-1.002 1z" fill-rule="nonzero"/></svg>
                     
-                        <b><a style= "color: ${findColor(pokemon, type)};">About</a></b> 
+                        <a style= "color: ${findColor(pokemon, type)};">About</a></b> 
                         <b><a style= "color: ${findColor(pokemon, type)};">Base Stats</a></b>
-                        <b><a style= "color: ${findColor(pokemon, type)};">Evolution</a></b>
-                        <b><a style= "color: ${findColor(pokemon, type)};">Moves</a></b>
+                        <div class="dropdown">
+                            <b><a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style= "color: ${findColor(pokemon, type)};">
+                                More
+                                </a>
+                            <ul class="dropdown-menu">
+                                <li><button class="dropdown-item" type="button" style= "color: ${findColor(pokemon, type)};">Evolution</button></li>
+                                <li><button class="dropdown-item" type="button" style= "color: ${findColor(pokemon, type)};">Moves</button></li>
+                                <li><button class="dropdown-item" type="button" style= "color: ${findColor(pokemon, type)};">Something else here</button></li>
+                            </ul>
+                        </div>
             
                     
                     <svg class="arrow" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.022 14.999v3.251c0 .412.335.75.752.75.188 0 .375-.071.518-.206 1.775-1.685 4.945-4.692 6.396-6.069.2-.189.312-.452.312-.725 0-.274-.112-.536-.312-.725-1.451-1.377-4.621-4.385-6.396-6.068-.143-.136-.33-.207-.518-.207-.417 0-.752.337-.752.75v3.251h-9.02c-.531 0-1.002.47-1.002 1v3.998c0 .53.471 1 1.002 1z" fill-rule="nonzero"/></svg>    
