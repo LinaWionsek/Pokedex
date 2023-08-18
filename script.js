1efd3665039f546b044ddf9b8d8b4b2c45be42e0
fshippman <fshippman@yahoo.com>
2023-08-18 10:14:08 +0200

    Added closing function to close detail window if clicking outside of it

--- script.js
+++ script.js
@@ -101,6 +101,15 @@ function lastPokemon(pokemonId) { //das i was übergeben wird ist Pokemon id i -
     renderPokedex(pokemon);
 }
 
+window.addEventListener('mouseup',function(event){
+        var detail = document.getElementById('detailID');
+        if(!(event.target.closest("#detailID"))){
+            detail.style.display = 'none';
+            closePokedex();
+        }
+    });
+  
+
 function createPokemonCardHTML(pokemon) {
     
     let type = findFirstType(pokemon);
@@ -111,12 +120,12 @@ function createPokemonCardHTML(pokemon) {
 
     return /*html*/ `
 
-    <div class="detail" style = "background-color: ${findColor(pokemon, type)}">
+    <div class="detail" id="detailID" style = "background-color: ${findColor(pokemon, type)}">
            
                     
                 
                 <div class="detail-header">
-                    <div class="close-card">
+                    <div onclick="closePokedex()" class="close-card">
                         <svg class="arrow" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>
                     </div>
                     <div class="pokemonCard-id h5">#${i}</div>
