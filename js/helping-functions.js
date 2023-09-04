e9bf610d58ed0ace24d0d07e43af326e05fcd8a2
fshippman <fshippman@yahoo.com>
2023-09-04 21:07:50 +0200

    testsuchfunktion funktioniert

--- js/helping-functions.js
+++ js/helping-functions.js
@@ -1,3 +1,35 @@
+async function getSearchedPokemon() {
+    let search_query = document.getElementById('searchQuery').value;
+    // let url = 'https://pokeapi.co/api/v2/pokemon/' + searchedPokemon
+    // let pokemon = await fetchApiReturnAsJson(url);
+    // renderSearchedPokemon(pokemon);
+    let newUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000'
+    // console.log(newUrl)
+    let data = await fetchApiReturnAsJson(newUrl);
+    
+
+  
+    for (let i = 0; i < data.results.length; i++) {
+        let tester = data.results[i].name
+        // console.log(tester)
+        if (tester.match(search_query)){
+            console.log(tester.match(search_query))
+           
+        }
+   
+    // let newstuff = data.results.find(record => record.name === searchedPokemon)
+}}
+
+// let newstuff = data.results.find(record => record.name === searchedPokemon)
+// console.log(newstuff)
+
+
+// function renderSearchedPokemon(pokemon) {
+//     document.getElementById('content').innerHTML = /*html*/ `<div id="pokedex" class="dialog-bg d-none"></div>`
+//     document.getElementById('content').innerHTML += createPreviewCardHTML(pokemon);
+//   }
+
+
 function fixNumber(nr) {
     let fix = nr.toFixed(2);
     return fix;
