e0d8839e35af26816938c15f1008fa3427735252
fshippman <fshippman@yahoo.com>
2023-09-05 19:36:32 +0200

    added searchfunction for number

--- js/helping-functions.js
+++ js/helping-functions.js
@@ -1,37 +1,58 @@
 async function getSearchedPokemon() {
+    // prevents new loading pokemons on scroll
     isAlreadyLoading = true
     let results = [];
+    // toLowerCase important because mobile phones start with uppercase letter and so function doesn't work
     let search_query = document.getElementById('searchQuery').value.toLowerCase();
-
-    if (search_query.length < 3) {
-        return;
-    }
-
     let newUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000'
-    // console.log(newUrl)
     let data = await fetchApiReturnAsJson(newUrl);
 
-
-    for (let i = 0; i < data.results.length; i++) {
-
-        let names = data.results[i].name
-        let inputMatches = names.match(search_query)
-        if (inputMatches) {
-            results.push(inputMatches)
+    // isNaN = is not a Number (proof if it is text)
+    if (isNaN(search_query)) {
+        // if it is a text, proof if the value more than 3 letters long
+        if (search_query.length < 3) {
+            return;
         }
-        // let newstuff = data.results.find(record => record.name === searchedPokemon)
+        searchByName(data, results, search_query)
+    } else {
+        searchByNumber(data, results, search_query)
     }
+
     document.getElementById('content').innerHTML = /*html*/ `<div id="pokedex" class="dialog-bg d-none"></div>`
     for (let i = 0; i < results.length; i++) {
         let result = results[i];
-        let url = 'https://pokeapi.co/api/v2/pokemon/' +  result['input']
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + result
         let pokemon = await fetchApiReturnAsJson(url);
         renderSearchedPokemon(pokemon);
     }
 }
 
-// let newstuff = data.results.find(record => record.name === searchedPokemon)
-// console.log(newstuff)
+function searchByNumber(data, results, search_query) {
+    for (let i = 0; i < data.results.length; i++) {
+        let urls = data.results[i].url
+        // split url in segments at the slash (/)
+        let segments = urls.split('/')
+        // 6. segment is individual pokemon id: e.g 1: (/v2/pokemon/1)
+        let pokemonID = segments[6]
+        console.log(pokemonID)
+        if (pokemonID === search_query) {
+            results.push(pokemonID)
+           break //stops loop 
+        }
+    }
+}
+
+
+function searchByName(data, results, search_query) {
+    for (let i = 0; i < data.results.length; i++) {
+        let names = data.results[i].name
+        let inputMatches = names.match(search_query)
+        console.log(inputMatches)
+        if (inputMatches) {
+            results.push(inputMatches['input'])
+        }
+    }
+}
 
 
 function renderSearchedPokemon(pokemon) {
